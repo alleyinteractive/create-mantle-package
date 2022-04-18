c2a6177e5e9fa6c1692fc437b4fcfeaf467b04a7
Sean Fisher <srtfisher@gmail.com>
2022-04-18 11:24:56 -0400

    Fixing testcase

--- tests/test-example-test.php
+++ tests/test-example-test.php
@@ -1,7 +1,8 @@
 <?php
 namespace Create_Mantle_Package\Tests;
 
-use Mantle\Testkit\TestCase;
+use Mantle\Testkit\Test_Case;
+
 class Test_Example_Test extends TestCase {
 	public function test_example_test() {
 		$this->assertTrue( class_exists( \Create_Mantle_Package\Example_Service_Provider::class ) );
